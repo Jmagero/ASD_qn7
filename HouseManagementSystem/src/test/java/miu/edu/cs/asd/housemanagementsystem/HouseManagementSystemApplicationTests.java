401baa1eb5f99c95d5f4ca144e364d2acea959af
Jocyline Magero <jmagero54@gmail.com>
2024-06-04 09:59:55 -0500

    first commit

---
+++ HouseManagementSystem/src/test/java/miu/edu/cs/asd/housemanagementsystem/HouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.cs.asd.housemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
